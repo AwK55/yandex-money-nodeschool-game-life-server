d456f14ad40a345eaeebc02aaac98ff0dd4b1d23
Andrey Kiselev <aw.v.kiselev@gmail.com>
2017-10-30 15:49:47 +0300

    add request validation

--- src/main.js
+++ src/main.js
@@ -5,11 +5,10 @@ const WebSocket = require('ws');
 const LifeGame = require('../lib/LifeGameVirtualDom');
 const url = require('url');
 
-const server = http.createServer();
-
 let users = [];
 const wss = new WebSocket.Server({
-    server
+    port: 8080,
+    verifyClient: validateConnection
 });
 
 wss.broadcast = function broadcast(data) {
@@ -29,18 +28,41 @@ LifeGame.prototype.sendUpdates = function (data) {
 };
 
 let game = new LifeGame();
+wss.on('connection', function (ws, req) {
+    const token = url.parse(req.url, true).query.token
+    init(ws, token, game);
+    addWebsocketHandlers(ws);
+});
 
-wss.on('connection', function connection(ws, req) {
-    init(ws, req, game);
+function validateConnection(info) {
+    return validateUser(info.req.url);
+}
 
+function validateUser(reqUrl) {
+    const clientToken = url.parse(reqUrl, true).query;
+    return clientToken && clientToken.token && checkUserName(clientToken.token);
+}
+
+function init(ws, token, game) {
+    const newUser = addUser(token);
+    ws.send(JSON.stringify({
+        type: 'INITIALIZE',
+        data: {
+            state: game.state,
+            settings: game.settings,
+            user: newUser
+        }
+    }));
+}
+
+function addWebsocketHandlers(ws) {
     ws.on('message', function (msg) {
-        try{
+        try {
             const rData = JSON.parse(msg)
             if (rData.type == 'ADD_POINT') {
                 game.applyUpdates(rData.data);
             }
-        }
-        catch(err) {
+        } catch (err) {
             console.log(`Life is not a game, son :${err}`);
         }
     });
@@ -50,35 +72,10 @@ wss.on('connection', function connection(ws, req) {
     });
 
     ws.on('close', function (code, reason) {
-        if(!ws.token) console.log('Connection closed');
+        if (!ws.token) console.log('Connection closed');
         removeUser(ws.token)
         console.log(`User ${ws.token} has left`);
-    })
-
-
-});
-
-server.listen(8080, function listening() {
-    console.log('Listening on %d', server.address().port);
-});
-
-function init(ws, req, game) {
-    const clientToken = url.parse(req.url, true).query;
-    let newUser = null;
-    if (clientToken && clientToken.token) {
-        ws.token = clientToken.token;
-        newUser = addUser(clientToken.token);
-    }
-    if (!newUser) return ws.send('auth failed');
-
-    ws.send(JSON.stringify({
-        type: 'INITIALIZE',
-        data: {
-            state: game.state,
-            settings: game.settings,
-            user: newUser
-        }
-    }));
+    });
 }
 
 function getRandomColor(attempts = 5) {
@@ -96,7 +93,6 @@ function checkUserColor(color) {
 }
 
 function addUser(name) {
-    if (!checkUserName(name)) return false;
     const color = getRandomColor();
     if (!color) return false;
 
